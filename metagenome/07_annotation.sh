56848ac9c2c9cf36e2d57663e4ea02e18d494779
Julia Y. Hung <88062133+JuliaHung@users.noreply.github.com>
2025-05-26 12:11:41 +1000

    Update 07_annotation.sh

--- metagenome/07_annotation.sh
+++ metagenome/07_annotation.sh
@@ -1,7 +1,16 @@
-### Using Prokka for annotation ###
+### Using Prokka, GhostKOALA, and EggNOG-mapper for annotation ###
 
 #prokka
 alias prokka='apptainer run -B /pvol/:/pvol /pvol/data/sif/prokka.sif prokka'
 while read bin;do 
   prokka --outdir $bin --prefix $bin "../Binning/das_tool/combined_polished/DAS_out/combined_polished_DASTool_bins/${bin}.fa";
 done < ../gtdbtk/DAS_tool/combined_polished/combined_polished_topbins_list.txt
+
+#GhostKOALA
+## amino acid sequences in FASTA format were uploaded to the GhostKOALA Automatic KO assignment and KEGG mapping web service (https://www.kegg.jp/ghostkoala/)
+## the "Prokaryptes + Viruses" reference GENES dataset was selected
+
+#EggNOG-mapper V2
+## Protein sequences in FASTA format were uploaded to the EggNOG-mapper genome-wide functional annotation web service (http://eggnog-mapper.embl.de/) for annotation.
+## used eggNOG 5 database
+## 
